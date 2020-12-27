3a82aa42048a36ee03de9dd9912a84d9b7c721db
BoxiGod <DevirLand@gmail.com>
2020-12-27 16:24:45 +0300

    added check in post preview

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def find_articles():
 
     for article in soup.find_all('article', class_="post"):
         title = article.find('h2')
+        preview = article.find('div', class_="post__text")
         for word in keywords:
-            if word in title.text.lower():
+            if word in title.text.lower() or word in preview.text.lower():
                 link = title.find('a').get('href')
                 date = article.find('header').find('span', class_='post__time').text
                 print(f'{date} - {title.text[1:-1]} - {link}')
